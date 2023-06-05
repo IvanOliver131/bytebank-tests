bd221a1727e8673ed7279014f12f7725c706aa10
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2023-06-05 11:09:43 -0300

    test: add testings

--- src/routes.test.js
+++ src/routes.test.js
@@ -1,1 +1,13 @@
 import { render, screen } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
+import App from './paginas/Principal/App'
+
+describe('Rotas', () => {
+  it('Deve renderizar a rota principal', () => {
+    render(<App />, { wrapper: BrowserRouter })
+
+    const usuario = screen.getByText('Olá, Joana :)!')
+
+    expect(usuario).toBeInTheDocument()
+  })
+})
