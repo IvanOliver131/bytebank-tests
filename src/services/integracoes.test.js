62a77c4fc2c676aeed2306d73ec615b0087b6244
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2023-06-07 17:20:53 -0300

    test: add tests

--- src/services/integracoes.test.js
+++ src/services/integracoes.test.js
@@ -1,15 +1,36 @@
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-import App from '../paginas/Principal/App';
+import api from './api';
 import { buscaTransacoes } from '../services/transacoes';
 
-describe('Requisições para API', () => {
-  test('Deve retornar uma lista de transações', async () => {
-    const transacoes = await buscaTransacoes();
-    expect(transacoes).toHaveLength(3);
+jest.mock('./api');
 
-    render(<App />, { wrapper: BrowserRouter });
-    const transacao = await screen.findAllByText('Novembro');
-    transacao.forEach((transacao) => expect(transacao).toBeInTheDocument());
+const mockTransacao = [
+  {
+    id: 1,
+    transacao: 'Depósito',
+    valor: '100',
+    data: '08/05/1998',
+    mes: 'Maio',
+  },
+];
+
+const mockRequisicao = (retorno) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: retorno,
+      });
+    }, 200);
+  });
+};
+
+describe('Requisições para API', () => {
+  it('Deve retornar uma lista de transações', async () => {
+    api.get.mockImplementation(() => mockRequisicao(mockTransacao));
+
+    const transacoes = await buscaTransacoes();
+
+    expect(transacoes).toEqual(mockTransacao);
+    expect(api.get).toHaveBeenCalledWith('/transacoes');
+    expect(transacoes).toHaveLength(1);
   });
 });
